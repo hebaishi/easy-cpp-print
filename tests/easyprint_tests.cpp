09a14e2e81c81cb3bcc0485b4eb3afa5351a94c6
hebaishi <hebaishi.husam@gmail.com>
2016-02-14 21:01:50 +0000

    Add first test, trigger Travis CI

--- tests/easyprint_tests.cpp
+++ tests/easyprint_tests.cpp
@@ -1,8 +1,19 @@
 #define BOOST_TEST_MODULE Utility_function_tests
 #include <boost/test/unit_test.hpp>
 
+#include "easyprint"
 
-BOOST_AUTO_TEST_CASE( fieldListFromString_tests )
+template <typename T, typename DELIMITER>
+std::string easyprint_test(T&& container, DELIMITER d ){
+    std::stringstream ss;
+    print_line(ss, std::forward<T>(container), d);
+    return ss.str();
+};
+
+BOOST_AUTO_TEST_CASE( Print_Empty_container )
 {
+    default_delimiter d;
+    BOOST_CHECK( easyprint_test(std::vector<int>({}), d) == "[]\n" );
 
+    std::cout << easyprint_test(std::vector<int>({}), d) << std::endl;
 }
