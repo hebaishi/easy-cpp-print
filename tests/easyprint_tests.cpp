69c94521260ecc39fad4c00d6e79c8c672f9f83f
hebaishi <hebaishi.husam@gmail.com>
2016-02-15 00:54:16 +0000

    Add tests for multiple element containers

--- tests/easyprint_tests.cpp
+++ tests/easyprint_tests.cpp
@@ -37,5 +37,16 @@ BOOST_AUTO_TEST_CASE( Print_Single_Element_container )
     BOOST_CHECK_EQUAL( easyprint_test(std::pair<int, std::string>({1, "s"}), d), "(1, \"s\")\n" );
     BOOST_CHECK_EQUAL( easyprint_test(std::unordered_map<int, int>({ {1,2} }), d), "{(1, 2)}\n" );
     BOOST_CHECK_EQUAL( easyprint_test(std::unordered_map<int, std::string>({ {1,"s"} }), d), "{(1, \"s\")}\n" );
-    BOOST_CHECK_EQUAL( easyprint_test(std::tuple<int, int, std::string, int>({}), d), "(0, 0, \"\", 0)\n" );
+}
+
+BOOST_AUTO_TEST_CASE( Print_Multiplee_Element_container )
+{
+    default_delimiter d;
+    BOOST_CHECK_EQUAL( easyprint_test(std::vector<int>({3, 7}), d), "[3, 7]\n" );
+    BOOST_CHECK_EQUAL( easyprint_test(std::vector<std::string>({"6", "8"}), d), "[\"6\", \"8\"]\n" );
+    BOOST_CHECK_EQUAL( easyprint_test(std::set<int>({4, 9}), d), "{4, 9}\n" );
+    BOOST_CHECK_EQUAL( easyprint_test(std::set<std::string>({"f", "v"}), d), "{\"f\", \"v\"}\n" );
+    BOOST_CHECK_EQUAL( easyprint_test(std::map<int, int>({ {1,2}, {5,7} }), d), "{(1, 2), (5, 7)}\n" );
+    BOOST_CHECK_EQUAL( easyprint_test(std::map<int, std::string>({ {1,"s"}, {4, "t"} }), d), "{(1, \"s\"), (4, \"t\")}\n" );
+    BOOST_CHECK_EQUAL( easyprint_test(std::make_tuple(1, 5, "s", 9), d), "(1, 5, \"s\", 9)\n" );
 }
